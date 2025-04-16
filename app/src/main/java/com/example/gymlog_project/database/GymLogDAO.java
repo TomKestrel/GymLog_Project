ff506822bfb16220a4618cc9195a70ebd574da9a
TomKestrel <wootarkkim@gmail.com>
2025-04-16 00:35:23 -0700

    End of video 12

--- app/src/main/java/com/example/gymlog_project/database/GymLogDAO.java
+++ app/src/main/java/com/example/gymlog_project/database/GymLogDAO.java
@@ -1,5 +1,6 @@
 package com.example.gymlog_project.database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
@@ -17,4 +18,7 @@ public interface GymLogDAO {
 
     @Query("SELECT * FROM " + GymLogDatabase.GYM_LOG_TABLE + " ORDER BY date DESC")
     List<GymLog> getAllRecords();
+
+    @Query("SELECT * FROM " + GymLogDatabase.GYM_LOG_TABLE + " WHERE userId = :loggedInUserId ORDER BY date DESC")
+    List<GymLog> getRecordsbyUserId(int loggedInUserId);
 }
